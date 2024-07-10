d8c8dfe017ab9819939e04ccc70487a9b823f2bf
Chege2004 <chegemanasseh2@gmail.com>
2024-07-10 10:23:37 +0300

    Added the code

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
   // Write your code here!
 }
-
-function ternaryCheckCity(){
+  else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
   // Write your code here!
 }
-
-function switchOnCharmFromTip(){
+  else if (feet >2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
   // Write your code here!
+}
+  else {
+    return 'No can do.';
+  // Write your code here!
+}
+}
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
